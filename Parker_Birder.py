782fcb59f6711eb432c4b2879cabd204ceae956f
rvhirsch <rhirsch@pugetsound.edu>
2019-03-11 22:04:25 -0600

    added swiping ability

--- Parker_Birder.py
+++ Parker_Birder.py
@@ -1,5 +1,6 @@
 import wikipedia
 import string
+import random
 
 # Birder - run with: python Parker_Birder.py
 
@@ -15,7 +16,7 @@ def makedict(filename):
                 birds[stripped] = {}
                 maintype = stripped
             else: # rest of line is info to put in dict
-                birds[maintype][info[0].lower()] = info[1:]
+                birds[maintype][info[0]] = info[1:]
     return birds
 
 # this prints the entirety of the bird text file
@@ -25,7 +26,7 @@ def printdict(birds):
         print ("** "+ category +" **\n")
         for bird in sorted(birds[category]):
             info = ", ".join(birds[category][bird]) # join elements in list by ", "
-            print("%-30s\t%s" % (string.capwords(bird), info)) # format line
+            print("%-30s\t%s" % (bird, info)) # format line
         print("")
 
 # right now all this does is returns just the summary of the wiki page
@@ -46,23 +47,72 @@ def getwikisummary(birdname):
 
 # searches for bird in list or tells user name is invalid
 def getbirdinfo(searchingbird, birds):
-    searchingbird = searchingbird.strip().lower()
+    searchingbird = string.capwords(searchingbird.strip().lower())
     for category in sorted(birds):
         if searchingbird in birds[category]:
             # return getwikiinfo(searchingbird)
             return getwikisummary(searchingbird)
     return "NOT A VALID BIRD NAME"
 
-def main():
-    birds = makedict("birds.txt")
-    printdict(birds)
-
+def getbirdinfobyname(birds):
     user = raw_input("Do you want info on a bird? (y/n) ")
     while (user[0].lower() == "y"):
-        bird = raw_input("Enter bird name: ")
+        bird = raw_input("\nEnter bird name: ")
         print(getbirdinfo(bird, birds))
 
-        user = raw_input("Do you want info on another bird? (y/n) ")
+        user = raw_input("\nDo you want info on another bird? (y/n) ")
+
+def getrandombird(birds):
+    # get random category
+    categories = birds.keys()
+    category = categories[random.randint(0, len(categories)-1)]
+
+    # get random bird in category
+    birdlist = birds[category].keys()
+    bird = birdlist[random.randint(0, len(birdlist)-1)]
+
+    # print("category: "+ category + ", bird: "+ bird)
+
+    return bird
+
+def swipebirds(birds):
+    yesbirds = set()
+    nobirds = set()
+
+    while True:
+        bird = getrandombird(birds)
+        while (bird in yesbirds or bird in nobirds): # make sure bird not seen yet
+            bird = getrandombird(swipebirds)
+
+        print("Bird: " + bird)
+        user = raw_input("Press 'M' for more info, 'S' to skip, or 'X' to stop swiping: ")
+        while (user != "M" and user != "S" and user != "X"):
+            print("INVALID OPTION, TRY AGAIN")
+            user = raw_input("Press 'M' for more info, 'S' to skip, or 'X' to stop swiping: ")
+
+        if user.upper() == "M":
+            yesbirds.add(bird)
+            print(getwikisummary(bird) + "\n")
+            # print(getbirdinfo(bird, birds) + "\n")
+        elif user.upper() == "S":
+            nobirds.add(bird)
+            print("")
+        else: # 'X' case
+            nobirds.add(bird)
+            break
+
+    print("\nYour Stats:")
+    print ("\nBirds You Liked: " + ", ".join(list(yesbirds)))
+    print ("\nBirds You Didn't Like: " + ", ".join(list(nobirds)))
+
+
+def main():
+    birds = makedict("birds.txt")
+    # printdict(birds)
+
+    # getbirdinfobyname(birds)
+
+    swipebirds(birds)
 
     print("\nThank you for using Birdr. Goodbye!")
 
